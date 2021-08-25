46312c10e30f02c4cecd47c7a13c34a6b7b6c13c
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-08-25 17:56:00 +0800

    Revert to "deploy my app to github pages"

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AllStyles from './styles/StyleAll';
-import Typography from './styles/FontStuffs';
-
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <>
-   <AllStyles />
-    <Typography />
+  <React.StrictMode>
     <App />
-  </>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
