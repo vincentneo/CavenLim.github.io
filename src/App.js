97c1d52786f64946e62b92f30361782d8124a485
CavenLim <19020620@myrp.edu.sg>
2021-08-25 12:33:48 +0800

    deploy my app to github pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import NavBar from './Components/NavBar';
+import Home from './pages/Home';
+import TaskCowDetails from './pages/TaskCowDetails';
+import DogCommDetails from './pages/DogCommDetails';
+import RpAppDetails from './pages/RpAppDetails';
+import SolarEduDetails from'./pages/SolarEduDetails';
+import TaskCowPrivacy from'./pages/TaskCowPrivacy';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Footer from './Components/Footer';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+
+          <Route path="/dogcomm">
+            <DogCommDetails />
+          </Route>
+
+          <Route path="/solaredu">
+            <SolarEduDetails />
+          </Route>
+
+          <Route path="/taskcowprivacy">
+            <TaskCowPrivacy />
+          </Route>
+
+          <Route path="/rpapp">
+            <RpAppDetails />
+          </Route>
+
+          <Route path="/taskcow">
+            <TaskCowDetails />
+          </Route>
+
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+
+         
+
+        </Switch>
+        <Footer />
+
+      </Router>
+
+    </>
   );
 }
 
